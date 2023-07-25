29349fdbadcc1bb0587bc59d14286ef08566b38b
Furdui Robert <Furdui Robert@DESKTOP-E78TGOH>
2023-07-25 06:13:06 -0700

    Add project files.

---
+++ Lab6ex1/Dulap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6ex1
+{
+    internal class Dulap
+    {
+        public int inaltime;
+        public int latime;
+        public int lungime;
+
+        public int CalculeazaVolum()
+        {
+            return inaltime * latime * lungime;
+        }
+
+        public Dulap(int inaltime,int latime,int lungime)
+        {
+            this.inaltime = inaltime;
+            this.latime = latime;
+            this.lungime = lungime;
+        }
+    }
+}
---
+++ Lab6ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Lab6ex1;
+
+
+/* Ex 1
+Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+metoda care va calcula volumul acestuia.
+Folositi o clasa care va modela un dulap.
+ Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+ Cele trei campuri vor fi initializate cu ajutorul constructorului
+ Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+afisati rezultatul.*/
+
+//Instantiem un dulap
+
+Dulap dulap = new Dulap(10,5,10);
+
+//apelam metoda
+
+int volum = dulap.CalculeazaVolum();
+
+Console.WriteLine( "Volumul dulapului este: " + volum);
